ef2a7868fe61f6c93de65c4a83f8322a81b7e01a
Wilbelison Junior <meustudio@gmail.com>
2024-07-10 00:26:31 -0300

    counter fixed

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,33 @@
 import { Link } from "react-router-dom";
 import ProductCounter from "./ProductCounter";
 
+const cart = [
+  {
+    id: 0,
+    count: 1,
+  },
+  {
+    id: 1,
+    count: 2,
+  },
+];
+
+
 const ProductCard = ({ product }) => {
+
+  let productCount = 0;
+
+  cart.forEach((item) => {
+    if(item.id === parseInt(product.id)) {
+      productCount = item.count;
+    }
+  });
   return (
     <li className="ProductCard">
       <Link to={`/product/${product.slug}`} className="card-link">
         <div className="card-thumbnail" style={{backgroundImage: `url(${product.thumbnail})`}}>
           <div className="thumbnail-overlay"></div>
-          <ProductCounter productId={product.id} />
+          <ProductCounter productId={product.id} productCount={productCount} />
         </div>
         <span className="card-title">{product.title}</span>
         <span className="card-details">{product.details}</span>
--- src/components/ProductCounter.jsx
+++ src/components/ProductCounter.jsx
@@ -3,35 +3,16 @@ import { useState } from "react";
 import iconRemove from "../assets/images/icon-remove.svg";
 import iconAdd from "../assets/images/icon-add.svg";
 
-const ProductCounter = ({ productId }) => {
-  const cart = [
-    {
-      id: 0,
-      count: 1,
-    },
-    {
-      id: 1,
-      count: 2,
-    },
-  ];
-
-  let counter = 0;
-
-  cart.map((item) => {
-    console.log(item);
-    if (item.id === parseInt(productId)) {
-      counter = item.count;
-    }
-  });
-
+const ProductCounter = ({ productId, productCount }) => {
+  const [counter, setCounter] = useState(productCount);
+  
   return (
     <div className="ProductCounter active">
       <button
         className="button-remove"
         onClick={(e) => {
           e.preventDefault();
-          if (counter > 0) counter--;
-          console.log(counter);
+          if (counter > 0) setCounter(counter - 1);
         }}
       >
         <img src={iconRemove} alt="Remover" />
@@ -41,8 +22,7 @@ const ProductCounter = ({ productId }) => {
         className="button-add"
         onClick={(e) => {
           e.preventDefault();
-          counter++;
-          console.log(counter);
+          setCounter(counter + 1);
         }}
       >
         <img src={iconAdd} alt="Adicionar" />
