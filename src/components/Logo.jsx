5e721834354656483fea9f4de2f0736b56ace89a
Wilbelison Junior <meustudio@gmail.com>
2024-07-12 12:04:29 -0300

    change logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 
+import logo from "../assets/images/logo.svg";
+
 const Logo = () => {
   return (
-    <Link to="/" className="Logo" title="Início" >
-      iDega
+    <Link to="/" className="Logo" title="Home">
+      <img src={logo} alt="iDega" />
     </Link>
   );
 };
